84dd3e0adc6ab9c7be7c23ad179b3580c0d3c787
adam-ghafara <adam.ghafara.job@gmail.com>
2023-10-08 02:38:34 +0700

    Pembaruan Web Lagi

--- js/getjson.js
+++ js/getjson.js
@@ -1,14 +1,5 @@
 import {framework} from "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"
+import {webdata} from "./load/load.js"
+import {pdfviewer} from "./pdf/viewer.js"
 
-const frameworkUrl = 'https://example.com/framework.js'; // Replace with the URL of the framework you want to load
-const proposal = 'https://example.com/data.json'; // Replace with the URL of the JSON data you want to load
-
-loadFrameworkAndJSON(frameworkUrl, jsonDataUrl)
-  .then((jsonData) => {
-    console.log('Framework and JSON data loaded successfully.');
-    console.log('JSON Data:', jsonData);
-  })
-  .catch((error) => {
-    console.error('Failed to load framework and/or JSON data:', error);
-  });
 
