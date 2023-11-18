a24c5f268bc2ad1e9770c762a1d9cdf24f0ca362
adam-ghafara <adam.ghafara.job@gmail.com>
2023-11-18 20:59:07 +0700

    Update (SignUp) : Mengupdate frontend untuk signUp dan signIn dan Update js signUp

--- js/signup.js
+++ js/signup.js
@@ -3,6 +3,11 @@ const apiUrl = 'https://api.example.com/signup';
 const fullnameInput = document.getElementById('fullname');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
+const journalInput = document.getElementById('journal_status');
+const submitButton = document.getElementById('submit');
+
+
+signUpForm.AddEventListener('submit', async (event) => {
 
 // Create a FormData object and append form data
 const signupObject = new signUpForm();
@@ -11,16 +16,22 @@ for (const key in signUpForm) {
 }
 
 // Use the fetch function to send the data to the API
+
 fetch(apiUrl, {
   method: 'POST',
   body: formDataObject,
 })
   .then(response => response.json())
   .then(data => {
-    console.log('Success:', data);
+    if(data.status === true) {
+        window.location.href = '../pages/signin.html';
     // Handle success response
+        console.log('SignUp Sukses! Sekarang kalian dapat login menggunakan akun yang kalian buat.', data);
+    }
   })
   .catch(error => {
     console.error('Error:', error);
     // Handle error
   });
+}
+)
