a7f27c5256f3ddd4d6ae36f4ce8885291d57e62b
adam-ghafara <adam.ghafara.job@gmail.com>
2023-11-27 21:05:45 +0700

    Fix (js) : Perbaikan beberapa js

--- js/getlastnew.js
+++ js/getlastnew.js
@@ -1,5 +1,30 @@
 const apiUrl = 'https://asia-southeast2-gis-moni.cloudfunctions.net/berkatauto-signUp';
-const articleForm = document.getElementById('articleForm');
+// const articleForm = document.getElementById('articleForm');
 const titleInput = document.getElementById('title');
-const contentInput = document.getElementById('content');
-const submitButton = document.getElementById('submit');
+// const contentInput = document.getElementById('content');
+// const submitButton = document.getElementById('submit');
+const loadnewsButton = document.getElementById('loadnews');
+
+articleForm.addEventListener('loadnews', async (event) => {
+    event.preventDefault();
+    const title = titleInput.value;
+    // const content = contentInput.value;
+
+    // Kirim data artikel ke API
+    // fetch('https://asia-southeast2-gis-moni.cloudfunctions.net/berkatauto-postArticle', {
+    //     method: 'POST',
+    //     body: JSON.stringify({ title, content }),
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         if (data.status === true) {
+    //             // Redirect user to user.html upon successful login
+    //             window.location.href = '../pages/blog-single.html';
+    //         } else {
+    //             errorMessage.textContent = 'Error: '; // pesan kesalahan
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+});
