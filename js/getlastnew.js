aa61f47919b680fcb71f6e11aeae23468a7e235f
adam-ghafara <adam.ghafara.job@gmail.com>
2023-11-27 21:00:16 +0700

    Update (js) : Update JS untuk getlastnew.js

---
+++ js/getlastnew.js
@@ -0,0 +1,5 @@
+const apiUrl = 'https://asia-southeast2-gis-moni.cloudfunctions.net/berkatauto-signUp';
+const articleForm = document.getElementById('articleForm');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+const submitButton = document.getElementById('submit');
