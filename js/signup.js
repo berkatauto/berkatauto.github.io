d03f7d20e6ed37539b156b4a1c6ba12bacee547e
adam-ghafara <adam.ghafara.job@gmail.com>
2024-01-29 22:40:50 +0700

    Update signup.html

--- js/signup.js
+++ js/signup.js
@@ -1,40 +1,26 @@
-// Replace these values with your actual form data and API endpoint
-const apiUrl = 'https://asia-southeast2-gis-moni.cloudfunctions.net/berkatauto-signUp';
-const signUpForm = document.getElementById('signUpForm');
-const fullnameInput = document.getElementById('fullname');
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const journalInput = document.getElementById('journal_bool');
-const submitButton = document.getElementById('submit');
+function signup() {
+  var fullname = document.getElementById("fullname").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var role = "user"
 
-
-signUpForm.AddEventListener('submit', async (event) => {
-
-// Create a FormData object and append form data
-const signupObject = new signUpForm();
-for (const key in signUpForm) {
-  signupObject.append(key, signUpForm[key]);
-}
-
-// Use the fetch function to send the data to the API
-
-fetch(apiUrl, {
-  method: 'POST',
-  body: formDataObject,
-})
-  .then(response => response.json())
-  .then(data => {
-    if(data.status === true) {
-        window.location.href = '../pages/signin.html';
-    // Handle success response
-        alert('Sign Up Sukses! Sekarang kalian dapat login menggunakan akun yang kalian buat.')
-        document.getElementById('signupInformation').innerHTML = '<p>Sign Up Sukses! Sekarang kalian dapat login menggunakan akun yang kalian buat.</p>';
-        console.log('SignUp Sukses! Sekarang kalian dapat login menggunakan akun yang kalian buat.', data);
+  var data = {
+    fullname: fullname,
+    username: username,
+    password: password,
+    role: role
+  }
+  fetch("https://asia-southeast2-spheric-entity-401507.cloudfunctions.net/signup", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
     }
+  }).then((response) => {
+    return response.json()
   })
-  .catch(error => {
-    console.error('Error:', error);
-    // Handle error
-  });
+  .catch((error) => {
+    console.log(error)
+  })
 }
-)
+document.getElementById("submitButton").addEventListener("click", signup)
